bf040cfd2890046e77393a20b36b73bf61ca01c3
Sophia Castellarin <scastellarin@anaconda.com>
2019-08-14 15:03:35 -0500

    Create class hierarchy for packages

--- grumble/models/environment.py
+++ grumble/models/environment.py
@@ -3,8 +3,13 @@ import copy
 
 class Environment(object):
 
+    def __init__(self, path, packages):
+        self.path = path
+        self.packages = packages
+
     def serialize(self, fn, filetype="yaml"):
         pass
 
     def load(self, fn):
         pass
+
---
+++ grumble/models/package.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+from conda.models.match_spec import MatchSpec
+
+
+class Package(ABC):
+
+    def __init__(self, name):
+        self.name = name
+
+    @abstractmethod
+    def to_matchspec(self):
+        return MatchSpec(self.name)
+
+
+class CondaPackage(Package):
+
+    def __init__(self, name, version, build_string, channel, subdir):
+        super(CondaPackage, self).__init__(name)
+        self.version = version
+        self.build_string = build_string
+        self.channel = channel
+        self.subdir = subdir
+
+    def to_matchspec(self):
+        return MatchSpec(
+            name=self.name, version=self.version, build=self.build_string,
+            channel=self.channel, subdir=self.subdir
+        )
+
+
+class PipPackage(Package):
+
+    def __init__(self, name, version):
+        super(PipPackage, self).__init__(name)
+        self.version = version
+
+    def to_matchspec(self):
+        pass
