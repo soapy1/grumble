22b4dcee8c6b0f3d9013c60dddb039a21c064206
Sophia Castellarin <scastellarin@anaconda.com>
2019-08-19 16:12:38 -0500

    Refactor useful function out into utils

--- tests/instantiator/test_conda.py
+++ tests/instantiator/test_conda.py
@@ -1,36 +1,12 @@
+from ..utils import package_is_installed
+
 from grumble.models import environment, package
 from grumble.instantiators import conda
 
 import tempfile
 import shutil
 from unittest import TestCase
-
-from conda.models.enums import PackageType
-from conda.api import PrefixData
-from conda.common.io import dashlist
 from conda_env import pip_util
-
-
-def package_is_installed(prefix, spec, pip=None):
-    prefix_recs = tuple(PrefixData(prefix).query(spec))
-    if len(prefix_recs) > 1:
-        raise AssertionError("Multiple packages installed.%s"
-                             % (dashlist(prec.dist_str() for prec in prefix_recs)))
-    is_installed = bool(len(prefix_recs))
-    if is_installed and pip is True:
-        assert prefix_recs[0].package_type in (
-            PackageType.VIRTUAL_PYTHON_WHEEL,
-            PackageType.VIRTUAL_PYTHON_EGG_MANAGEABLE,
-            PackageType.VIRTUAL_PYTHON_EGG_UNMANAGEABLE,
-            PackageType.VIRTUAL_PYTHON_EGG_LINK,
-        )
-    if is_installed and pip is False:
-        assert prefix_recs[0].package_type in (
-            None,
-            PackageType.NOARCH_GENERIC,
-            PackageType.NOARCH_PYTHON,
-        )
-    return is_installed
 
 
 class TestCondaInstatniator(TestCase):
